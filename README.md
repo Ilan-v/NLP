ce3e0a2cf27f64cae32e556228530546cd4dcd5a
Ilan <ilan.vasilevsky@gmail.com>
2022-04-12 11:22:15 +0300

    Final changes to code

--- README.md
+++
@@ -1,4 +0,0 @@
-# NLP
-This is our project <br>
-wow so **cool** <br>
-$$ c^2 = a^2 + b^2 .$$
--- test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-print('hello git')
-print('error')
-print('error')
-print('error')
-print('error')
-print('error')
-print('error')
-print('u cant see me')
